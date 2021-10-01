5327e3fbbf5b0da9ddac6f50e777a1aeedcad878
Documenter.jl <documenter@juliadocs.github.io>
2021-10-01 03:36:26 +0000

    build based on eb11f47

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TrigPolys","page":"Introduction","title":"TrigPolys","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"TrigPolys.jl is a package for fast manipulation of trigonometric polynomials.","category":"page"},{"location":"","page":"Introduction","title":"Introduction","text":"p(x)  = c + sum_k=1^2n a_kcos(kx) + b_ksin(kx) quad x in 0pi","category":"page"}]
+}
