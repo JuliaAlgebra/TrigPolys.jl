34eb409896badcc3717542017a6adaadcc5a486b
Benoît Legat <benoit.legat@gmail.com>
2023-07-04 16:47:44 +0200

    Cite D17 book

--- src/TrigPolys.jl
+++ src/TrigPolys.jl
@@ -12,7 +12,14 @@ export pad_to, truncate
         as::VT   # sin coefficients
     end
 
-Represents a trigonometric polynomial by its coefficients.
+Represents a *Hermitian trigonometric polynomial* by its coefficients.
+The vectors `ac` and `as` should have the same length, that we call `n` in this
+docstring.
+This represent the following function
+```julia
+R(ω) = a0 + sum(ac[k] * cos(k * ω) + as[k] * sin(k * ω) for k in 1:n)
+```
+which is a polynomial in the variable `x = cos(ω)`.
 """
 struct TrigPoly{T<:AbstractFloat, VT<:AbstractVector{T}}
     a0::T    # Constant coefficient
